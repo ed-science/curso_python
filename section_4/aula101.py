c301173f684244eb5719dd32564c846a85533893
ed-science <edsg.eds@gmail.com>
2023-02-14 22:43:22 -0300

    atualizacao-aulas-202302014

---
+++ section_4/aula101.py
@@ -0,0 +1,22 @@
+# Exercício - Adiando execução de funções
+def soma(x, y):
+    return x + y
+
+
+def multiplica(x, y):
+    return x * y
+
+
+def criar_funcao(funcao, *args):
+    x = args[0]
+    
+    def interna(*args):
+        return funcao(*args)    
+    return interna
+
+
+
+
+
+soma_com_cinco = criar_funcao(soma, 5)
+multiplica_por_dez = criar_funcao(multiplica, 10)
